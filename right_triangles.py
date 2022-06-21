7ef834baf1931075f8548151c07745894f776498
Mario Fiorino <87363023+MarioFiorino@users.noreply.github.com>
2022-06-21 13:19:52 +0200

    Update and rename right_triangle.py to right_triangles.py

--- right_triangles.py
+++ right_triangles.py
@@ -2,7 +2,7 @@
 
 #!usr/bin/bash python
 
-def right_triangle(arr_sides):
+def right_triangles(arr_sides):
 
   l = []
 
@@ -25,4 +25,5 @@ arr = [13,3,4,5,12,13,5,12,4,4,2]
 arr = set (arr) # delete duplicates
 arr = list (arr)
 arr = sorted(arr,reverse = True)
-print(right_triangle(arr)) # check:  https://en.wikipedia.org/wiki/Tree_of_primitive_Pythagorean_triples
+print(right_triangles(arr))# out : [[13, 12, 5], [5, 4, 3]]
+# check:  https://en.wikipedia.org/wiki/Tree_of_primitive_Pythagorean_triples
